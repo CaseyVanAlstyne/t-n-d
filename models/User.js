b5078d0d94da77e47d49e83ce161d622c9c55714
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-08-01 11:36:09 -0400

    server info added into scripts

---
+++ models/User.js
@@ -0,0 +1,24 @@
+// This file pulls in required dependencies, creates a Schema to represent a User, defining fields and types as objects of the Schema, and exports the model so we can access it outside of this file
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = User = mongoose.model("users", UserSchema);

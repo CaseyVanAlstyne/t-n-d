3da511231aed8d1b1b713df0a22478f0f8a32a39
Casey VanAlstyne <caseywvanalstyne@gmail.com>
2020-08-03 14:33:01 -0400

    had to change import in store js and file names as there was a collision of naming conventions

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import { Provider } from "react-redux";
 import store from "./store";
-
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";

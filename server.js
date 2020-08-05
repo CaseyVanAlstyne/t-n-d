2da60bc84f8b3b56503ad33cd2eb55e000e187a7
WillV1 <wmvanhook@gmail.com>
2020-08-04 21:07:57 -0400

    Fixed server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-app.use("/api", users);
+
 // DB Config
 // const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
@@ -31,7 +31,8 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 // Routes
-app.use("/api/users", users);
+app.use("/api", users);
+// app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));

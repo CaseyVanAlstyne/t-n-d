e0532bfff72d4e93c271583e04db7c1bd248d565
SeanW <sean.walmer@outlook.com>
2020-08-08 10:36:58 -0400

    added state to the dashboard component and then changed to sending stat to the statblock

--- models/User.js
+++ models/User.js
@@ -38,7 +38,11 @@ const userSchema = new Schema({
     default: 0,
   },
   tasks: {
-    quests: [],
+    quests: [{
+        name: {type: String, required: true,},
+        experience: { type: Number, required: true, default: 0,},
+        date: {type: Date, default: Date.now,}
+      }],
     dailies: [],
   },
   //   we can put an empty array to house the user's todos. This would be empty upon initial access to the application.

69301fe1030d2a4e8d08bad5291516538cda612f
WillV1 <wmvanhook@gmail.com>
2020-08-13 20:11:43 -0400

    Add id to dashboard and schema

--- models/User.js
+++ models/User.js
@@ -45,6 +45,7 @@ const userSchema = new Schema({
       name: { type: String, required: true },
       experience: { type: Number, required: true, default: 20 },
       date: { type: Date, default: Date.now },
+      id: {type: String, required: true}
     },
   ],
   // dailies: [],
